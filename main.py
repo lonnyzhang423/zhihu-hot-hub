581a09fe9c8ba4ac052cfd028d6c655514f47774
lonnyzhang <lonnyzhang423@gmail.com>
2021-01-07 23:17:42 +0800

    update

--- main.py
+++ main.py
@@ -13,55 +13,81 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(level=logging.INFO)
 
-HOT_URL = "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50"
+HOT_SEARCH_URL = 'https://www.zhihu.com/api/v4/search/top_search'
+HOT_QUESTION_URL = 'https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50'
 
 retries = Retry(total=2,
                 backoff_factor=0.1,
                 status_forcelist=[k for k in range(400, 600)])
 
+headers={
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+}
 
 def getContent(url: str) -> str:
     try:
         with requests.session() as s:
             s.mount("http://", HTTPAdapter(max_retries=retries))
             s.mount("https://", HTTPAdapter(max_retries=retries))
-            r = s.get(url)
-            return r.text
+            return s.get(url, headers=headers).text
     except:
         log.error(traceback.format_exc())
 
 
-def parseTopics(content):
-    """解析话题
+def parseSearchList(content):
+    """解析热搜
     """
-    def topic(item):
+    def search(item):
         info = {}
-        info['id'] = item['id']
-        info['title'] = item['title']
-        info['content'] = item['content']
-        info['url'] = item['url']
+        info['title'] = item['display_query']
+        info['url'] = 'https://www.zhihu.com/search?q={}'.format(item['query'])
         return info
 
     result = []
     try:
-        arr = json.loads(content)
+        arr = json.loads(content)['top_search']['words']
         if arr:
-            result = [topic(item) for item in arr]
+            result = [search(item) for item in arr]
     except:
         log.error(traceback.format_exc())
 
     return result
 
 
-def generateArchiveReadme(items):
+def parseQuestionList(content):
+    """解析热搜
+    """
+    def question(item):
+        info = {}
+        target = item['target']
+        info['title'] = target['title']
+        info['url'] = 'https://www.zhihu.com/question/{}'.format(target['id'])
+        return info
+
+    result = []
+    try:
+        arr = json.loads(content)['data']
+        if arr:
+            result = [question(item) for item in arr]
+    except:
+        log.error(traceback.format_exc())
+
+    return result
+
+
+def generateArchiveReadme(searches, questsions):
     """生成归档readme
     """
-    def topic(item):
+    def liMd(item):
         return '1. [{}]({})'.format(item['title'], item['url'])
 
-    topics = '暂无数据'
-    if items:
-        topics = '\n'.join([topic(item) for item in items])
+    searchMd = '暂无数据'
+    if searches:
+        searchMd = '\n'.join([liMd(item) for item in searches])
+
+    questionMd = '暂无数据'
+    if questsions:
+        questionMd = '\n'.join([liMd(item) for item in questsions])
 
     readme = ''
     with open('README_archive.template', 'r') as f:
@@ -71,20 +97,25 @@ def generateArchiveReadme(items):
     now = util.currentTimeStr()
     readme = readme.replace("{date}", date)
     readme = readme.replace("{updateTime}", now)
-    readme = readme.replace("{topics}", topics)
+    readme = readme.replace("{searches}", searchMd)
+    readme = readme.replace("{questions}", questionMd)
 
     return readme
 
 
-def generateTodayReadme(items):
+def generateTodayReadme(searches, questsions):
     """生成今日readme
     """
-    def topic(item):
+    def liMd(item):
         return '1. [{}]({})'.format(item['title'], item['url'])
 
-    topics = '暂无数据'
-    if items:
-        topics = '\n'.join([topic(item) for item in items])
+    searchMd = '暂无数据'
+    if searches:
+        searchMd = '\n'.join([liMd(item) for item in searches])
+
+    questionMd = '暂无数据'
+    if questsions:
+        questionMd = '\n'.join([liMd(item) for item in questsions])
 
     readme = ''
     with open('README.template', 'r') as f:
@@ -92,7 +123,8 @@ def generateTodayReadme(items):
 
     now = util.currentTimeStr()
     readme = readme.replace("{updateTime}", now)
-    readme = readme.replace("{topics}", topics)
+    readme = readme.replace("{searches}", searchMd)
+    readme = readme.replace("{questions}", questionMd)
 
     return readme
 
@@ -109,27 +141,33 @@ def handleArchiveMd(md):
     util.writeText(file, md)
 
 
-def handleRawContent(content: str):
+def handleRawContent(content: str, filePrefix: str):
     log.debug('raw content:%s', content)
-    name = util.currentDateStr()+'.json'
+    name = '{}-{}.json'.format(filePrefix, util.currentDateStr())
     file = os.path.join('raw', name)
     util.writeText(file, content)
 
 
 def run():
-    # 获取数据
-    content = getContent(HOT_URL)
-    topics = parseTopics(content)
+    # 热搜数据
+    searchContent = getContent(HOT_SEARCH_URL)
+    searches = parseSearchList(searchContent)
+    # 问题数据
+    questionContent = getContent(HOT_QUESTION_URL)
+    questions = parseQuestionList(questionContent)
 
     # 最新数据
-    todayMd = generateTodayReadme(topics)
+    todayMd = generateTodayReadme(searches, questions)
     handleTodayMd(todayMd)
     # 归档
-    archiveMd = generateArchiveReadme(topics)
+    archiveMd = generateArchiveReadme(searches, questions)
     handleArchiveMd(archiveMd)
+
     # 原始数据
-    raw = json.dumps(json.loads(content), ensure_ascii=False)
-    handleRawContent(raw)
+    raw = json.dumps(json.loads(searchContent), ensure_ascii=False)
+    handleRawContent(raw, 'hot-search')
+    raw = json.dumps(json.loads(questionContent), ensure_ascii=False)
+    handleRawContent(raw, 'hot-question')
 
 
 if __name__ == "__main__":
